3a6593ff96ccd941d8ff7836b9e225ac52823619
Anguelos Nicolaou <anguelos.nicolaou@gmail.com>
2011-11-18 18:25:11 +0100

    rails new kagglito

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Kagglito::Application.config.secret_token = '548e9a88169c9692e5b1235e90160c9ba2dbc354a9554372757b79b01f187932cdf3318edb726c8569fbc390690a8bff7eca5120d475379e8ba4074a84a98037'
