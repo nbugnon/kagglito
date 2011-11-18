74f982786315fbff6a1f00d91b0fb02174c50651
Anguelos Nicolaou <anguelos.nicolaou@gmail.com>
2011-11-18 23:57:38 +0100

    Added create shell scripts

---
+++ vendor/create_scuffolds.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+git checkout scuffolding
+rails generate scuffold AnnotatedChalenge groundtruth:binary chalenge:binary
+rails generate scuffold Dataset description:string
+rails generate scuffold Competition description:string
+rails generate scuffold User description:string login:string password:string email:string administrator:boolean
+rails generate scuffold Evaluator description:string src:binary
+
+
